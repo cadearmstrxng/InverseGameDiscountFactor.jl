c1b7dc7b7294496c0687fde1c3cbbbdd279e52cd
cadearmstrxng <caderade.a@gmail.com>
2024-07-08 11:28:11 -0500

    Added some needed code for the package

---
+++ src/CostUtils.jl
@@ -0,0 +1,19 @@
+module CostUtils
+
+import JuMPUtils
+export symbol, normalize
+
+symbol(s::Symbol) = s
+symbol(s::JuMP.Containers.DenseAxisArrayKey) = only(s.I)
+
+function normalize(weights)
+    total = sum(weights)
+    map(w -> w / total, weights)
+end
+
+function namedtuple(associative_array)
+    nt_keys = Tuple(symbol.(keys(associative_array)))
+    NamedTuple{nt_keys}(typle(values(associative_array)))
+end
+
+end
--- src/InverseGameDiscountFactor.jl
+++ src/InverseGameDiscountFactor.jl
@@ -1,5 +1,9 @@
 module InverseGameDiscountFactor
 
-
+include("DynamicsModelInterface.jl")
+include("JuMPUtils.jl")
+include("CostUtils.jl")
+include("ForwardOptimalControl.jl")
+include("ForwardGame.jl")
 
 end # module InverseGameDiscountFactor
