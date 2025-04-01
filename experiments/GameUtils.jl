170493e425fd1cf5f7e78a2bf23b5c8f1580b661
Ryan Park <ryanjpark03@utexas.edu>
2025-04-01 11:06:42 -0500

    wip

--- experiments/GameUtils.jl
+++ experiments/GameUtils.jl
@@ -149,7 +149,7 @@ function InD_collision_avoidance(
 
             context_state[Block(i)][myopic ? 5 : 4] * mean_target + 
             context_state[Block(i)][myopic ? 6 : 5] * control +
-            # context_state[Block(i)][myopic ? 7 : 6] * safe_distance_violation +
+            context_state[Block(i)][myopic ? 7 : 6] * safe_distance_violation +
 
             # costs unique to cars
             (i > 2 ? context_state[Block(i)][myopic ? 8 : 7] * lane_center : 0.0)
